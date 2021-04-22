46cba2d60992d5c4aeefd2b321475df16295fffc
steff-dz <stefaniemarie.dz@gmail.com>
2021-04-22 23:05:43 +0200

    fetching data from sanity working

--- pages/vault.js
+++ pages/vault.js
@@ -1,5 +1,43 @@
-const Vault = () => {
-  return <h1>The Vault</h1>;
+import client from "../client";
+import groq from "groq";
+import { useState, useEffect } from "react";
+
+export async function getStaticProps() {
+  const query = groq`
+    {
+        "tricks": *[_type == 'tricks']{title,
+        'categories': categories[]-> title,
+        }
+    }
+    
+    `;
+
+  const data = await client.fetch(query);
+
+  return {
+    props: {
+      tricks: data,
+    },
+  };
+}
+
+const Vault = ({ tricks }) => {
+  console.log(tricks);
+  //   const [tricksData, setTricksData] = useState("");
+
+  //   useEffect(() => {
+  //     if (tricks.length) {
+  //       setTricksData(tricks);
+  //     } else {
+  //       console.log("no tricks here yet bro");
+  //     }
+  //   }, [tricksData]);
+
+  return (
+    <div>
+      <h1>The Vault</h1>
+    </div>
+  );
 };
 
 export default Vault;
