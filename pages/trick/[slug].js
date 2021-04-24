b3265d5a5b86dba2eff6524e604a016bea132a36
steff-dz <stefaniemarie.dz@gmail.com>
2021-04-24 20:04:44 +0200

    got dynamic trick page working

--- pages/trick/[slug].js
+++ pages/trick/[slug].js
@@ -1,5 +1,9 @@
 import client from "../../client";
 import groq from "groq";
+import BlockContent from "@sanity/block-content-to-react";
+import { MainWrapper } from "../../components/MainWrapper";
+import { PageTitle } from "../../components/PageTitle";
+import styled from "styled-components";
 
 export function getStaticPaths() {
   return {
@@ -13,7 +17,19 @@ export async function getStaticProps({ params }) {
   //Try and fetch data
 
   const query = groq`
-    *[_type == 'tricks' && slug.current == '${slug}'][0]
+    *[_type == 'tricks' && slug.current == '${slug}'][0]{
+      title,
+      slug,
+      mainImage{
+        asset->{
+          _id,
+          url
+        }
+      },
+      body,
+      'categories': categories[]-> title,
+
+    }
   `;
 
   const data = await client.fetch(query);
@@ -27,8 +43,48 @@ export async function getStaticProps({ params }) {
 }
 
 const TrickPage = ({ trick }) => {
-  console.log(trick);
-  return <h1>This is the Trick Page</h1>;
+  //console.log(trick.mainImage.asset.url);
+
+  return (
+    <MainWrapper>
+      <PageTitle>{trick && trick.title}</PageTitle>
+      <ArticleBase>
+        {trick && <img src={trick.mainImage.asset.url} />}
+        <h2>Description:</h2>
+        <p>{trick && <BlockContent id="descrip" blocks={trick.body} />}</p>
+      </ArticleBase>
+    </MainWrapper>
+  );
 };
 
+const ArticleBase = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 60%;
+  margin: 0 auto;
+  margin-top: 20px;
+  gap: 20px;
+
+  h2 {
+    font-size: ${(props) => props.theme.fontSizes[8]};
+  }
+
+  p {
+    font-size: ${(props) => props.theme.fontSizes[7]};
+  }
+
+  img {
+    width: 500px;
+  }
+`;
+
 export default TrickPage;
+
+// import imageUrlBuilder from "@sanity/image-url";
+
+// const builder = imageUrlBuilder(client);
+
+// function urlFor(source) {
+//   return builder.image(source);
+// }
