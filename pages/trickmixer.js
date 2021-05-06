53db6dea65bf33167d86a9440d7b0b15a69d59f7
steff-dz <stefaniemarie.dz@gmail.com>
2021-05-06 15:51:49 +0200

    restyling trick mix pg

--- pages/trickmixer.js
+++ pages/trickmixer.js
@@ -85,130 +85,71 @@ const TrickMixer = ({ tricks }) => {
 
   return (
     <>
-      <NavigationBar />
-      <Wrapper>
-        <main>
-          <PageTitle style={{ paddingBottom: "2rem" }}>Trick Mixer</PageTitle>
-          <SectionFormBase>
-            <p>
-              If you need ideas for a run, we can help you with that! Select the
-              level of tricks you want, then hit the GO button. <br />
-              <br />
-              We will grab a random set of six tricks for you that you can put
-              together in a single run. Of course, feel free to skip some, add
-              others, and do them in any order you want!
-            </p>
-            <FormBase onSubmit={(e) => testSubmit(e)}>
-              <Input
-                type={"checkbox"}
-                id={"beginnerLvl"}
-                name={"beginnerLvl"}
-                value={"Beginner"}
-                title={"Beginner Level"}
-                handleInput={handleInput}
-              />
-              <Input
-                type={"checkbox"}
-                id={"intermediateLvl"}
-                name={"intermediateLvl"}
-                value={"Intermediate"}
-                title={"Intermediate Level"}
-                handleInput={handleInput}
-              />
-              <Input
-                type={"checkbox"}
-                id={"advancedLvl"}
-                name={"advancedLvl"}
-                value={"Advanced"}
-                title={"Advanced Level"}
-                handleInput={handleInput}
-              />
-              <button type="submit">GO</button>
-            </FormBase>
-          </SectionFormBase>
-          <SectionTricksBase>
-            {trickData && (
-              <button onClick={() => setDisplayTricks(!displayTricks)}>
-                Checkout My Mix
-              </button>
-            )}
-            <div className="trick-container">
-              {displayTricks &&
-                trickData.map((trick) => (
-                  <div className="trick-card" key={trick.slug.current}>
-                    {trick.title}
-                  </div>
-                ))}
-            </div>
-          </SectionTricksBase>
-        </main>
-      </Wrapper>
+      <IndexWrapper>
+        <ImageContainer></ImageContainer>
+        <NavigationBar />
+      </IndexWrapper>
+      <MainBase>
+        <StyledPageTitle>Trick Mixer</StyledPageTitle>
+        <SectionBase>
+          <article id="instruction-container"></article>
+          <article id="form-container"></article>
+        </SectionBase>
+      </MainBase>
     </>
   );
 };
 
-const SectionFormBase = styled.section`
-  border: 1px solid grey;
-  width: 90%;
-  margin: 0 auto;
-  display: flex;
-  justify-content: center;
-  font-size: ${(props) => props.theme.fontSizes[6]};
-  font-family: ${(props) => props.theme.textFont};
-  p {
-    border: 1px solid pink;
-    width: 45%;
-    margin: 1rem;
-    padding: 2rem 3rem;
-    background-color: ${(props) => props.theme.white};
-    color: ${(props) => props.theme.black};
-
-    line-height: 50px;
-    border-radius: 10px;
-    text-align: left;
-  }
+const IndexWrapper = styled(Wrapper)`
+  overflow: hidden;
+  position: relative;
 `;
 
-const FormBase = styled.form`
-  width: 45%;
-  margin: 1rem;
-  padding: 2rem 3rem;
-  background-color: ${(props) => props.theme.white};
-  border-radius: 10px;
+const ImageContainer = styled.div`
+  background-image: url("/images/rail.jpeg");
+  height: 100%;
+  width: 50%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: absolute;
+  left: 0;
+  opacity: 0.5;
+`;
+
+const MainBase = styled.main`
+  height: 100vh;
+  width: 100vw;
+  position: absolute;
+  top: 0;
+`;
+
+const StyledPageTitle = styled(PageTitle)`
+  z-index: 30;
+  margin-top: 5%;
+  font-size: 8rem;
+`;
+
+const SectionBase = styled.section`
+  margin: 0 auto;
+  border: 1px solid pink;
+  width: 90%;
+  height: 70%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
 
-  button {
-    width: 20%;
-    font-size: 3rem;
+  #instruction-container {
+    border: 1px solid black;
+    width: 400px;
+    height: 300px;
+  }
+
+  #form-container {
+    border: 1px solid yellow;
+    width: 400px;
+    height: 300px;
+    align-self: flex-end;
   }
 `;
 
-const SectionTricksBase = styled.section`
-  border: 1px solid lightsalmon;
-  width: 90%;
-  min-height: 20vh;
-  margin: 0 auto;
-  padding: 2rem 0;
-
-  button {
-    width: 20%;
-    font-size: 3rem;
-    margin: 1rem;
-  }
-
-  .trick-container {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 30px;
-    margin: 0 5rem;
-  }
-
-  .trick-card {
-    background-color: white;
-    font-size: 3rem;
-    padding: 1rem 1.5rem;
-    text-align: center;
-  }
-`;
 export default TrickMixer;
